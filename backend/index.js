06bfe7cbd9f21ae90d6e85a864a66041b296cab0
SusalSandeepa <susalsandeepa2002@gmail.com>
2025-11-16 19:33:59 +0530

    Add simple GET and POST endpoints to validate Express server functionality

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,34 @@ import express from 'express';
 
 const app = express()
 
+// Middleware to parse JSON bodies
+app.use(express.json())
+
+app.get("/",
+    (req,res)=>{
+        console.log(req.body)
+        console.log('Get request received')
+        
+        res.json(
+            {
+                message: "Hello from server!"
+            }
+        )
+    }
+)
+
+app.post("/",
+    (req, res)=>{
+        console.log('Post request received')
+
+        res.json(
+            {
+                message: "Post request received!"
+            }
+        )
+    }
+)
+
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
 });
