59da054c8dcfaafa3b07f80a13f5c9df45b81660
SusalSandeepa <susalsandeepa2002@gmail.com>
2025-11-18 19:49:01 +0530

    Create User Controller and User Route

---
+++ backend/controllers/userController.js
@@ -0,0 +1,23 @@
+import User from "../models/user.js";
+
+export function createUser(req,res){
+    const user = new User({
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: req.body.password
+    })
+
+    user
+		.save()
+		.then(() => {
+			res.json({
+				message: "User created successfully",
+			});
+		})
+		.catch(() => {
+			res.json({
+				message: "Failed to create user",
+			});
+		});
+}
