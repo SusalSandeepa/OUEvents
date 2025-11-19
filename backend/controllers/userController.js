8abad8412906b9753bd8fcf3e7cbb061bec0a56f
SusalSandeepa <susalsandeepa2002@gmail.com>
2025-11-19 21:53:15 +0530

    Integrate bcrypt for secure password hashing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,15 @@
 import User from "../models/user.js";
+import bcrypt from "bcryptjs";
 
 export function createUser(req,res){
+
+	const hashedPassword = bcrypt.hashSync(req.body.password, 10); // Hash the password with a salt round of 10
+
     const user = new User({
         email: req.body.email,
         firstName: req.body.firstName,
         lastName: req.body.lastName,
-        password: req.body.password
+        password: hashedPassword
     })
 
     user
