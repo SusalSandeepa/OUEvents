378082876a9cede55027913512f721e5be1f3aad
kavindidilhara <kavidil28@gmail.com>
2025-12-30 00:19:57 +0530

    Update navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -17,8 +17,8 @@ export default function Navbar() {
   return (
     <nav className="flex items-center justify-between w-full px-8 py-3 bg-white border-b border-gray-200">
       {/* LEFT */}
-      <div className="flex items-center space-x-3">
-        <img src={logo} alt="Logo" className="w-12 h-12" />
+      <div className="flex items-center space-x-4">
+        <img src={logo} alt="Logo" className="w-auto h-20" />
         <span className="text-2xl font-bold text-gray-800">OUEvents</span>
       </div>
 
@@ -30,12 +30,12 @@ export default function Navbar() {
       </div>
 
       {/* RIGHT */}
-      <div className="flex items-center space-x-4">
+      <div className="flex items-center space-x-8">
         <button className="border border-[#7a1d1a] text-[#7a1d1a] px-4 py-1.5 rounded-lg font-medium hover:bg-[#7a1d1a] hover:text-white transition">
           Log In
         </button>
 
-        <div className="w-10 h-10 rounded-full bg-[#7a1d1a] text-white flex items-center justify-center text-xl">
+        <div className="w-9 h-9 rounded-full bg-[#7a1d1a] text-white flex items-center justify-center text-xl">
           <span>👤</span>
         </div>
       </div>
