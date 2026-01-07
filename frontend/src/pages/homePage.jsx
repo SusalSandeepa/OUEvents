4c4400b4212df7cb0f497ab660e89175662c47e0
SusalSandeepa <susalsandeepa2002@gmail.com>
2026-01-07 10:48:03 +0530

    add routes for homepage

---
+++ frontend/src/pages/homePage.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+import Hero from "../components/Hero"
+
+export default function HomePage(){
+    return(
+        <>
+            <Navbar />
+            <Hero />
+            <Footer />
+        </>
+    )
+}
+
+
